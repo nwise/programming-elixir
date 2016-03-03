90a5244d304efdab14b8c3fbe4c1c265dbf1122e
Nathan Wise <nathanwise@gmail.com>
2016-03-03 13:02:54 -0500

    Moving reduce examples out of lists

--- lists/mylists.exs
+++ lists/mylists.exs
@@ -24,31 +24,4 @@ defmodule MyList do
   # Same as #sum without accumulator
   def sum1([]), do: 0
   def sum1([head | tail]), do: head + sum1(tail)
-
-  # This can be used to build #sum
-  def reduce([], value, _) do
-    value
-  end
-  def reduce([head | tail], value, func) do
-    reduce(tail, func.(head, value), func)
-  end
-
-  # applies the function to each element and then sums the result
-  # ex. MyList,mapsum [1,2,3], &(&1*&1)  ==> [1,4,9] ==> 14
-  def mapsum([], _func), do: 0
-  def mapsum([head | tail], func) do
-    func.(head) + mapsum(tail, func)
-  end
-
-  # returns the element with the maximum value in th list
-  def max_l([]) do
-    0
-  end
-  def max_l([head | tail]) do
-    if head > max_l(tail) do
-      head
-    else
-      max_l(tail)
-    end
-  end
 end
